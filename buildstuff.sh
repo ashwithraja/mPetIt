640edfc7a714dd09446c0b0c9175c396a0a2cd54
sagar <sagar@mobilitas.co>
2012-12-18 17:35:42 +0530

    Added buildAll script to build all projects at a time. Signed-off-by: sagar <sagar@mobilitas.co>

---
+++ buildstuff.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+FAILED=0
+cd ./mSiS-app
+ant clean
+ant debug
+if [ "$?" = 1 ]; then
+    echo "Application failed!"
+    FAILED=1
+fi
+cd ..
+
+# cd ./FridgeMagnetTest
+# ant clean
+# ant test
+# if [ "$?" = 1 ]; then
+	# echo "FridgeMagnetTest build failed!"
+    # FAILED=1
+# fi
+# cd ..
+exit $FAILED
